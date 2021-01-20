eeeb8f6f92d52cb7fb99412f7d7a9d0a2e577c20
Şeymanur Diktaş <seymanurdiktas@gmail.com>
2021-01-20 15:27:53 +0300

    Add project files.

---
+++ Kampintro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Kampintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "kategori";
+            int ogrenciSayisi = 32000000;
+            double faizOrani = 1.45;
+            bool sistemeGiris = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+            if (sistemeGiris == true)
+
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
